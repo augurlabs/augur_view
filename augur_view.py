4c49a20822f8e014e301b8787e2270c4c43ef539
Ulincsys <28362836a@gmail.com>
2021-01-16 20:40:57 -0600

    Initial commit

---
+++ augur_view.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+import urllib.request, json
+from pathlib import Path
+
+app = Flask(__name__)
+
+URL = "http://zephyr.osshealth.io:5222/api/unstable"
+
+@app.route('/')
+def augur_view():
+    query = request.args.get('q')
+
+    try:
+        cache_file = Path("cache/repos.json")
+        if cache_file.is_file():
+            with open('cache/repos.json') as f:
+                data = json.load(f)
+        else:
+            with urllib.request.urlopen(URL + "/repos") as url:
+                data = json.loads(url.read().decode())
+                with open('cache/repos.json', 'w') as f:
+                    json.dump(data, f)
+
+        if(query is not None):
+            results = []
+            for repo in data:
+                if query in repo["repo_name"]:
+                    results.append(repo)
+            data = results
+        page = render_template('index.html', title="Repos - Augur View", repos=data)
+    except Exception as err:
+        print(err)
+        page = render_template('index.html', title="Repos - Augur View")
+
+    return page
