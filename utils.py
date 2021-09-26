daeb219dfcafa3dca7378e835aebe2f84b7b615a
Ulincsys <28362836a@gmail.com>
2021-09-26 04:57:25 -0500

    reimplemented dynamic caching system, added report processing, refactoring

--- utils.py
+++ utils.py
@@ -3,78 +3,75 @@ from concurrent.futures import ThreadPoolExecutor
 from flask import render_template
 import urllib.request, json, os, math, yaml, urllib3, time, logging
 
-# URL for all endpoint calls, probably won't be hardcoded for much longer
-# URL = "http://zephyr.osshealth.io:5222/api/unstable"
-# cacheDir = "cache/"
-
+# load configuration files and initialize globals
 configFile = "config.yml"
-
-settings = { 'approot': "/", 'caching': "static/cache/", 'serving': "default.osshealth.io", 'paginationOffset': 25, 'reports': "reports.yml" }
-
+settings = { 'approot': "/", 'caching': "static/cache/", 'cache_expiry': 172800, 'serving': "default.osshealth.io", 'paginationOffset': 25, 'reports': "reports.yml" }
 reports = None
-
 report_requests = {}
 
+# Initialize logging
 format = "%(asctime)s: %(message)s"
-
 logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
 
+""" ----------------------------------------------------------------
+"""
 def loadSettings():
     try:
         with open(configFile) as file:
             global settings
             settings = yaml.load(file, Loader=yaml.FullLoader)
     except Exception as err:
-        print("Error reading application settings from [" + configFile + "], default settings kept:")
-        print(err)
+        logging.error(f"An exception occurred settings from [{configFile}], default settings kept:")
+        logging.error(err)
         try:
             with open(configFile, 'w') as file:
-                print("Attempting to generate default config.yml")
+                logging.info("Attempting to generate default config.yml")
                 yaml.dump(settings, file)
         except Exception as ioErr:
-            print("Error creating default config:")
-            print(ioErr)
+            logging.error("Error creating default config:")
+            logging.error(ioErr)
 
+""" ----------------------------------------------------------------
+"""
 def getSetting(key):
-    if key == 'approot':
-        if settings[key] == "private":
-            with open(".app_root") as f:
-                settings[key] = f.readline()
     return settings[key]
 
+loadSettings()
+
+""" ----------------------------------------------------------------
+"""
 def loadReports():
     global reports
     try:
         with open(getSetting("reports")) as file:
             reports = yaml.load(file, Loader=yaml.FullLoader)
+            id = -1
+            for report in reports:
+                for image in reports[report]:
+                    image['id'] = id = id + 1
     except Exception as err:
-        print("Error reading reports endpoints from [" + getSetting("reports") + "]:")
-        print(err)
-
-loadSettings()
+        logging.error(f"An exception occurred reading reports endpoints from [{getSetting('reports')}]:")
+        logging.error(err)
 
 loadReports()
 
-"""
-try:
-    rootPath = Path(".app_root")
-    if rootPath.is_file():
-        with open(".app_root") as f:
-            approot = f.readline()
-    else:
-        approot = "/"
-except Exception as err:
-    print("Error reading application root from .app_root:")
-    print(err)
-    print("Application root set to [/]")
-    approot = "/"
-"""
-
 cache_files_requested = []
 
+""" ----------------------------------------------------------------
+"""
 def cacheFileExists(filename):
     cache_file = Path(filename)
-    if cache_file.is_file() or filename in cache_files_requested:
+    if cache_file.is_file():
+        if(getSetting('cache_expiry') > 0):
+            cache_file_age = time.time() - cache_file.stat().st_mtime
+            if(cache_file_age > getSetting('cache_expiry')):
+                try:
+                    cache_file.unlink()
+                    logging.info(f"Cache file {filename} removed due to expiry")
+                    return False
+                except Exception as e:
+                    logging.error("Error: cache file age exceeds expiry limit, but an exception occurred while attempting to remove")
+                    logging.error(e)
         return True
     else:
         return False
@@ -82,13 +79,17 @@ def cacheFileExists(filename):
 def stripStatic(url):
     return url.replace("static/", "")
 
+""" ----------------------------------------------------------------
+toCacheFilename:
+    Takes an endpoint string or
+"""
 def toCacheFilename(endpoint):
     return getSetting('caching') + endpoint.replace("/", ".").replace("?", "_").replace("=", "_") + '.agcache'
 
 def toCacheURL(endpoint):
     return stripStatic(getSetting('caching')) + endpoint.replace("/", ".").replace("?", "_").replace("=", "_") + '.agcache'
 
-"""
+""" ----------------------------------------------------------------
 requestJson:
     Attempts to load JSON data from cache for the given endpoint.
     If no cache file is found, a request is made to the URL for
@@ -110,7 +111,7 @@ def requestJson(endpoint):
     filename = toCacheFilename(endpoint)
     requestURL = getSetting('serving') + "/" + endpoint
     try:
-        if cacheFileExists(filename) and not filename in cache_files_requested:
+        if cacheFileExists(filename):
             with open(filename) as f:
                 data = json.load(f)
         else:
@@ -122,14 +123,16 @@ def requestJson(endpoint):
             cache_files_requested.remove(filename)
         return data
     except Exception as err:
-        print(err)
+        logging.error("An exception occurred while fulfilling a json request")
+        logging.error(err)
 
+""" ----------------------------------------------------------------
+"""
 def requestPNG(endpoint):
     filename = toCacheFilename(endpoint)
     requestURL = getSetting('serving') + "/" + endpoint
-    # print(requestURL)
     try:
-        if cacheFileExists(filename) and not filename in cache_files_requested:
+        if cacheFileExists(filename):
             return toCacheURL(endpoint)
         else:
             urllib.request.urlretrieve(requestURL, filename)
@@ -137,25 +140,37 @@ def requestPNG(endpoint):
             cache_files_requested.remove(filename)
         return toCacheURL(endpoint)
     except Exception as err:
-        print(err)
+        logging.error("An exception occurred while fulfilling a png request")
+        logging.error(err)
 
-def download(url, cmanager, filename, image_cache):
-    image_cache[(stripStatic(filename))] = {}
-    if cacheFileExists(filename) and not filename in cache_files_requested:
-        image_cache[(stripStatic(filename))]['exists'] = True
+""" ----------------------------------------------------------------
+"""
+def download(url, cmanager, filename, image_cache, image_id, repo_id = None):
+    image_cache[image_id] = {}
+    image_cache[image_id]['path'] = stripStatic(filename)
+    if cacheFileExists(filename):
+        image_cache[image_id]['exists'] = True
         return
     response = cmanager.request('GET', url)
     if "json" in response.headers['Content-Type']:
-        logging.info("WARN: unexpected json response in image request for repo")
-        logging.info(response.data.decode('utf-8'))
-        image_cache[(stripStatic(filename))]['exists'] = False
+        logging.warn(f"repo {repo_id}: unexpected json response in image request")
+        logging.warn(f"  response: {response.data.decode('utf-8')}")
+        image_cache[image_id]['exists'] = False
         return
     if response and response.status == 200:
-        image_cache[(stripStatic(filename))]['exists'] = True
-        with open(filename, 'wb') as f:
-            f.write(response.data)
+        image_cache[image_id]['exists'] = True
+        try:
+            with open(filename, 'wb') as f:
+                f.write(response.data)
+        except Exception as err:
+            logging.error("An exception occurred writing a cache file to disk")
+            logging.error(err)
 
+""" ----------------------------------------------------------------
+"""
 def requestReports(repo_id):
+    if(repo_id in report_requests.keys()):
+        return
     report_requests[repo_id] = {}
     report_requests[repo_id]['complete'] = False
     if reports is None:
@@ -167,11 +182,10 @@ def requestReports(repo_id):
         connection_mgr = urllib3.PoolManager(maxsize=size)
         thread_pool = ThreadPoolExecutor(size)
         threadPools.append(thread_pool)
-        for url in reports[report]:
-            filename = toCacheFilename(f"{url}?repo_id={str(repo_id)}")
-            url = f"{getSetting('serving')}/{url}?repo_id={str(repo_id)}"
-            logging.info(url)
-            thread_pool.submit(download, url, connection_mgr, filename, reportImages)
+        for image in reports[report]:
+            filename = toCacheFilename(f"{image['url']}?repo_id={repo_id}")
+            image_url = f"{getSetting('serving')}/{image['url']}?repo_id={repo_id}"
+            thread_pool.submit(download, image_url, connection_mgr, filename, reportImages, image['id'], repo_id)
 
     # Wait for all connections to resolve, then clean up
     for thread_pool in threadPools:
@@ -181,7 +195,7 @@ def requestReports(repo_id):
     # Remove the request from the queue when completed
     report_requests[repo_id]['complete'] = True
 
-"""
+""" ----------------------------------------------------------------
 renderRepos:
     This function renders a list of repos using a given view, while passing query
     data along. This function also processes pagination automatically for the
@@ -222,10 +236,14 @@ def renderRepos(view, query, data, page = None, filter = False, pageSource = "re
     x = PaginationOffset * (page - 1)
     data = data[x: x + PaginationOffset]
 
-    print("Pages", pages, "Page", page, "Data", len(data))
-
     return render_template('index.html', body="repos-" + view, title="Repos", repos=data, query_key=query, activePage=page, pages=pages, offset=PaginationOffset, PS=pageSource, api_url=getSetting('serving'), root=getSetting('approot'))
 
+def renderMessage(messageTitle, messageBody, title = None, redirect = None, pause = None):
+    return render_template('index.html', body="notice", title=title, messageTitle=messageTitle, messageBody=messageBody, api_url=getSetting('serving'), redirect=redirect, pause=pause)
+
+""" ----------------------------------------------------------------
+    No longer used
+"""
 # My attempt at a loading page
 def renderLoading(dest, query, request):
     cache_files_requested.append(request)
