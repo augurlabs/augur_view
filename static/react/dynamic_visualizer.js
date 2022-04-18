9e87970ad1fcba9664e199cecb457823da176851
JoelAHilliard <hilliardjoel05@gmail.com>
2022-04-18 18:58:22 -0500

    added a second graph

--- static/react/dynamic_visualizer.js
+++ static/react/dynamic_visualizer.js
@@ -1,10 +1,25 @@
 
 const {useEffect, useState} = React;
 const {Line, LineChart,CartesianGrid, XAxis, YAxis, Tooltip, Legend} = Recharts;
+function formatDate(date) {
+  var d = new Date(date),
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate(),
+    year = d.getFullYear();
+
+  if (month.length < 2) 
+    month = '0' + month;
+  if (day.length < 2) 
+    day = '0' + day;
+
+  return [year, month, day].join('-');
+}
 function DynamicVisualizer(){
   const [myData, setMyData] = useState(null)
 
-  function getDataForRepoWithDates(){
+  const [forkData, setForkData] = useState(null);
+
+  function getStarDataForRepoWithDates(){
     fetch('http://augur.chaoss.io/api/unstable/repo-groups/25000/stars')
     .then(function(response) {
       return response.json();
@@ -17,6 +32,22 @@ function DynamicVisualizer(){
       setMyData(tempData)
     });
   }
+  function getForkData(){
+    fetch('http://augur.chaoss.io/api/unstable/repo-groups/25000/forks')
+    .then(function(response) {
+      return response.json();
+    }).then(function(data) {
+      console.log(data)
+      for(var i =0;i<data.length;i++){
+        data[i].date = formatDate(data[i].date);
+      }
+      setForkData(data)
+    });
+  }
+
+
+
+
   useEffect(() => {
     // fetch('http://augur.chaoss.io/api/unstable/repos')
     // .then(function(response) {
@@ -25,7 +56,8 @@ function DynamicVisualizer(){
     //   console.log(data)
     //   setMyData(data);
     // });
-    getDataForRepoWithDates()
+    getStarDataForRepoWithDates()
+    getForkData();
   }, [])
   function stringDate(dateS){
     var tempD = new Date(dateS);
@@ -35,8 +67,8 @@ function DynamicVisualizer(){
   if( myData){
     return(
     //   This is for all the data
-    <div style={{width:"100%", justifyContent:"center"}}>
-     
+    <div style={{width:"100%", justifyContent:"center", display:"flex", "flex-flow":"row"}}>
+      <div id="graph1">
         <h1>Stars over time for {myData[0].repo_name}</h1>
           <LineChart width={500} height={500} data={myData} style={{margin:"100px auto"}} >
 
@@ -47,6 +79,19 @@ function DynamicVisualizer(){
             <Legend />
             <Line type="monotone" dataKey="stars" stroke="#8884d8" />
         </LineChart>
+        </div>
+        <div id="graph2">
+        <h1>Forks over time for {myData[0].repo_name}</h1>
+          <LineChart width={500} height={500} data={forkData} style={{margin:"100px auto"}} >
+
+            <CartesianGrid />
+            <XAxis dataKey="date"/>
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="forks" stroke="#8884d8" />
+        </LineChart>
+        </div>
     </div>
     )
   }
