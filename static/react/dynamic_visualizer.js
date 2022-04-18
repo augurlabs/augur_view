aa731a1e595f11094283fb451a31a341f119e20e
nickblakewylie <nw5pp@mail.missouri.edu>
2022-04-18 18:40:50 -0500

    Got the date working for recharts

--- static/react/dynamic_visualizer.js
+++ static/react/dynamic_visualizer.js
@@ -10,7 +10,11 @@ function DynamicVisualizer(){
       return response.json();
     }).then(function(data) {
       console.log(data)
-      setMyData(data);
+      var tempData = []
+      for(var i = 0; i < data.length; i ++){
+        tempData.push(data[i])
+      }
+      setMyData(tempData)
     });
   }
   useEffect(() => {
@@ -23,17 +27,27 @@ function DynamicVisualizer(){
     // });
     getDataForRepoWithDates()
   }, [])
+  function stringDate(dateS){
+    var tempD = new Date(dateS);
+    console.log(tempD.getFullYear().toString())
+    return tempD.getFullYear().toString() +"-" + tempD.getMonth() + "-" + tempD.getDate();
+  }
   if( myData){
     return(
     //   This is for all the data
-      <LineChart width={800} height={500} data={myData}>
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="date" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Line type="monotone" dataKey="stars" stroke="#8884d8" />
-    </LineChart>
+    <div style={{width:"100%", justifyContent:"center"}}>
+     
+        <h1>Stars over time for {myData[0].repo_name}</h1>
+          <LineChart width={500} height={500} data={myData} style={{margin:"100px auto"}} >
+
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" tickFormatter={(tickS) => {return stringDate(tickS)}}/>
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="stars" stroke="#8884d8" />
+        </LineChart>
+    </div>
     )
   }
   return (
