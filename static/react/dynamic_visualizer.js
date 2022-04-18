32be4c1656a82904c122889794d277a571cfb37b
nickblakewylie <nw5pp@mail.missouri.edu>
2022-04-18 18:01:58 -0500

    I got some data to load into rechart

--- static/react/dynamic_visualizer.js
+++ static/react/dynamic_visualizer.js
@@ -1,22 +1,39 @@
 
 const {useEffect, useState} = React;
-const {Line, LineChart} = Recharts;
+const {Line, LineChart,CartesianGrid, XAxis, YAxis, Tooltip, Legend} = Recharts;
 function DynamicVisualizer(){
   const [myData, setMyData] = useState(null)
-  useEffect(() => {
-    fetch('http://augur.chaoss.io/api/unstable/repo-groups')
+
+  function getDataForRepoWithDates(){
+    fetch('http://augur.chaoss.io/api/unstable/repo-groups/25000/stars')
     .then(function(response) {
       return response.json();
     }).then(function(data) {
       console.log(data)
       setMyData(data);
     });
+  }
+  useEffect(() => {
+    // fetch('http://augur.chaoss.io/api/unstable/repos')
+    // .then(function(response) {
+    //   return response.json();
+    // }).then(function(data) {
+    //   console.log(data)
+    //   setMyData(data);
+    // });
+    getDataForRepoWithDates()
   }, [])
-  if(myData){
+  if( myData){
     return(
-      <div>
-        {myData[0].rg_name}
-      </div>
+    //   This is for all the data
+      <LineChart width={800} height={500} data={myData}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="date" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Line type="monotone" dataKey="stars" stroke="#8884d8" />
+    </LineChart>
     )
   }
   return (
